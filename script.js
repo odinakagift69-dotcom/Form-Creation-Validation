835ad854b86255304963d78619efcadb68210f76
Gift <odinakagift69@gmail.com>
2025-10-31 07:20:16 -0700

    Initial commit Form Creation and Vaildation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded" , function () {
     const form = document.getElementById("registration-form");
     const feedbackDiv = document.getElementById("form-feedback");
 
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
 
 
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded" , function () {
         const password = document.getElementById("password").ariaValueMax.trim();
 
         let isValid = true;
-        const messages =[];
+        const messages = [];
 
-        if (username.lenght < 3) {
+        if (username.length < 3) {
             isValid = false;
             messages.push("Username must be at least 3 characters long.");
         }
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded" , function () {
             messages.push("Password must be at least 8 characters long.");
         }
 
-        feedbackDiv.style.display ="block";
+        feedbackDiv.style.display = "block";
 
         if (isValid) {
             feedbackDiv.textContent= "Registration successful !";
